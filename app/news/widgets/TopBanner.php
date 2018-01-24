d718e1a478fc8d30d14ff23fba467bba5d6a9b25
LiChunHua <lichunhua@medatc.com>
2018-01-24 10:26:25 +0800

    x

--- app/news/widgets/TopBanner.php
+++ app/news/widgets/TopBanner.php
@@ -5,10 +5,16 @@ class TopBanner extends \yii\base\Widget
 {
     public function run()
     {
-        $result = \WS::getStaticData('news.banner.top.'.\WS::$app->area->id);
+        $result = \models\SiteSetting::get('news.banner.top', \WS::$app->area->id);
 
         return $this->render('top-banners.phtml', [
             'result'=>$result
         ]);
+    }
+
+    public function createNewUrl($id)
+    {
+        $id = trim($id, '#');
+        return \WS::$app->urlManager->createUrl(['news/default/view', 'id'=>$id]);
     }
 }
